747ce61825fb0574ca6f48ba1e8743c980d80d7f
Micael Popping <mmpopping@inf.ufpel.edu.br>
2020-05-18 23:50:25 -0300

    server/src/server.py

--- server/src/server.py
+++ server/src/server.py
@@ -6,16 +6,15 @@ class Server:
     def __init__(self, host, port):
         self.HOST = host
         self.PORT = port
-        self.server = None
 
     
     def start(self):
             # Start server.
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.HOST, self.PORT))
-        self.server.listen()
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((self.HOST, self.PORT))
+        server.listen()
             # Start news_clients thread.
-        t_news_clients = news_clients(self.server)
+        t_news_clients = news_clients(server)
         t_news_clients.start()
 
         print('Server start.')
