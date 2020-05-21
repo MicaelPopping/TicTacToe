03daa7cd103f1a506c4d09fd497de2b4b61875e6
Micael Popping <mmpopping@inf.ufpel.edu.br>
2020-05-21 00:26:35 -0300

    news_clients thread

--- server/src/news_clients.py
+++ server/src/news_clients.py
@@ -1,22 +1,28 @@
 from threading import Thread
+from client import Client
 
 class news_clients(Thread):
 
-    def __init__(self, server):
+    def __init__(self, server, clients):
         Thread.__init__(self)
         self.server = server
+        self.clients = clients
 
     def run(self):
 
-        current_client_id = 0
+        current_id = 0
             # Loop looking for new clients.
         while True:
-                # Awaiting new client.
+                # Finding and adding a new client to the client list.
             new_client, new_addr = self.server.accept()
-                #
-                # ...
+            self.clients.insert(Client(current_id, new_client, new_addr))
+                # Start listening client.
+            # ...
                 # Answers the client that he is connected.
-                # ...
-            print('Connected client.')
+            new_client.send(b'/connected')
                 # Increment the current client id.
-            current_client_id += 1
+            current_id += 1
+
+                # test code.
+            print('Connected client.')
+            #print(self.clients)
